9136eea5896ecfe8c45b9d94e307e1265a55b86d
SabbirIrfan <sabbirirfan29@gmail.com>
2024-01-25 10:50:57 +0600

    bug fix-- request mapping

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -2,12 +2,16 @@ package controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String home(){
+    public String home(Model model){
+        String name = "sabbir irfan chowdhury";
+        model.addAttribute("name", name);
+
         System.out.println("this is home");
         return "index";
     }
